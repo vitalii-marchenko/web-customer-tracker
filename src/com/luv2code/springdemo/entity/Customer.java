5bf70dd3575c069fc29431855b5ff877e14336e2
Vitaly Marchenko <vmarchenko@n-ix.com>
2020-04-24 15:51:58 +0300

    Add CUstomer Entity

---
+++ src/com/luv2code/springdemo/entity/Customer.java
@@ -0,0 +1,32 @@
+package com.luv2code.springdemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customer")
+@Data
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
