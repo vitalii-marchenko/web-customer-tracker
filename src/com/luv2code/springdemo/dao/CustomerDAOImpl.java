a02755bb44e8dec1e8618deb2be1a223ffb0b33c
Vitaly Marchenko <vmarchenko@n-ix.com>
2020-04-26 17:04:02 +0300

    Add small fix

--- src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -29,7 +29,7 @@ public class CustomerDAOImpl implements CustomerDAO {
     @Override
     public void saveCustomer(Customer customer) {
         Session currentSession = sessionFactory.getCurrentSession();
-        currentSession.save(customer);
+        currentSession.saveOrUpdate(customer);
     }
 
     @Override
--- src/main/webapp/WEB-INF/view/customer-form.jsp
+++ src/main/webapp/WEB-INF/view/customer-form.jsp
@@ -23,6 +23,9 @@
             <h3>Save Customer</h3>
 
             <form:form action="saveCustomer" modelAttribute="customer" method="post">
+
+                <form:hidden path="id"></form:hidden>
+
                 <table>
                     <tbody>
                         <tr>
